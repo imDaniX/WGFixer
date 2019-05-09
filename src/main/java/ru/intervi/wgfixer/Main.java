185a2c13fb3facc7830792393381cad571bf4b8a
imdanix <idanix@list.ru>
2019-05-10 00:41:04 +0300

    Saving some performance by redesigning string checks.

--- src/main/java/ru/intervi/wgfixer/Main.java
+++ src/main/java/ru/intervi/wgfixer/Main.java
@@ -46,12 +46,13 @@ public class Main extends JavaPlugin implements Listener {
 		if (
 				cmd.length < 4
 				|| 
-				!(((cmd[0].equals("/rg") || cmd[0].equals("/region") || cmd[0].equals("/regions") || cmd[0].equals("/worldguard:rg") || cmd[0].equals("/worldguard:region") || cmd[0].equals("/worldguard:regions"))
-				&&
-				(cmd[1].equals("removeowner") || cmd[1].equals("ro") || cmd[1].equals("removemember") || cmd[1].equals("rm") || cmd[1].equals("remmember") || cmd[1].equals("removemem") || cmd[1].equals("remmem") || cmd[1].equals("addowner") || cmd[1].equals("ao") || cmd[1].equals("addmember") || cmd[1].equals("am") || cmd[1].equals("addmem")))
+				!((cmd[0].equals("/rg") || cmd[0].equals("/region") || cmd[0].equals("/regions") || cmd[0].equals("/worldguard:rg") || cmd[0].equals("/worldguard:region") || cmd[0].equals("/worldguard:regions"))
 				&&
 				!cmd[3].equals("-a"))
 			) return;
+		cmd[1]=getAction(cmd[1]);
+		if(cmd[1]==null)
+			return;
 		User essUser = ess.getOfflineUser(cmd[3]);
 		if (essUser == null) { 
 			player.sendMessage(ChatColor.RED + "Игрока " + cmd[3] + " ещё небыло на сервере!");
@@ -66,42 +67,43 @@ public class Main extends JavaPlugin implements Listener {
 			player.sendMessage(ChatColor.RED + "Неизвестный регион " + cmd[2] + ".");
 			return;
 		}
+		if (!canAffect(rg, cmd[1], player))
+			return;
 		switch (cmd[1]) {
-			case "removeowner": case "ro": {
-				if (!canAffect(rg, "removeowner", player))
-					return;
+			case "removeowner": {
 				rg.getOwners().removePlayer(uuid);
 				player.sendMessage(ChatColor.GREEN + cmd[3] + " удалён из владельцев " + cmd[2] + ".");
 				break;
 			}
-			case "removemember": case "rm": case "remmember": case "removemem": case"remmem": {
-				if (!canAffect(rg, "removemember", player))
-					return;
+			case "removemember": {
 				rg.getMembers().removePlayer(uuid);
 				player.sendMessage(ChatColor.GREEN + cmd[3] + " удалён из участников " + cmd[2] + ".");
 				break;
 			}
-			case "addowner": case "ao": {
-				if (!canAffect(rg, "addowner", player))
-					return;
+			case "addowner": {
 				rg.getOwners().addPlayer(uuid);
 				player.sendMessage(ChatColor.GREEN + cmd[3] + " добавлен во владельцы " + cmd[2] + ".");
 				break;
 			}
-			case "addmember": case "am": case "addmem": {
-				if (!canAffect(rg, "addmember", player))
-					return;
+			case "addmember": {
 				rg.getMembers().addPlayer(uuid);
 				player.sendMessage(ChatColor.GREEN + cmd[3] + " добавлен в участники " + cmd[2] + ".");
 				break;
 			}
-			default: return;
 		}
 		if(!saveChanges(player.getWorld()))
 			player.sendMessage(ChatColor.RED+"При попытке сохранения была найдена ошибка! Попробуйте позже.");
 		event.setCancelled(true);
 	}
-
+	private String getAction(String arg) {
+		switch (arg) {
+			case "removeowner": case "ro": return "removeowner";
+			case "removemember": case "rm": case "remmember": case "removemem": case "remmem": return "removemember";
+			case "addowner": case "ao": return "addowner";
+			case "addmember": case "am": case "addmem": return "addmemver";
+			default: return null;
+		}
+	}
 	private boolean saveChanges(World world) {
 		try {
 			wg.getPlatform().getRegionContainer().get(BukkitAdapter.adapt(world)).saveChanges();
